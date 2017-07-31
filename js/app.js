83d5e5758a36b57e49305e1a056fdae9770fc1aa
Ilse Magali Keymolen Espinoza <ilsemagalikeymolenespinoza@MacBook-de-Martin.local>
2017-07-30 22:42:36 -0500

    mejorando menu nav hamburguesa

--- js/app.js
+++ js/app.js
@@ -35,6 +35,28 @@ function autoType(elementClass, typingSpeed){
 }
 
 $(document).ready(function(){
+  // navbar scrolling
+  $('a').each(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname
+    && this.hash.replace(/#/,'') ) {
+      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+       if ($target) {
+         var targetOffset = $target.offset().top;
+         $(this).click(function() {
+            $("#nav li a").removeClass("active");
+            $(this).addClass('active');
+           $('html, body').animate({scrollTop: targetOffset}, 1000);
+           return false;
+         });
+      }
+    }
+  });
+  // Collapsible menu
+  $("#collapsible-menu").on("click", function(){
+    $("#collapsible-menu").attr("aria-expanded")= "false";
+  })
   // Now to start autoTyping just call the autoType function with the
   // class of outer div
   // The second paramter is the speed between each letter is typed.
